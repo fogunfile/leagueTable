0ed1df281da8b1240f51e0e7c82f0cec32f80d0a
Foluso Ogunfile <folusoogunfile@yahoo.com>
2021-02-11 14:58:05 +0100

    Match Played table

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const table = document.querySelector("#t1");
+const matchPlayedTable = document.querySelector("#matches-played");
+const tbody = matchPlayedTable.querySelector("tbody");
+console.log(tbody);
+
 const point = document.querySelector("#pts1");
 const goals = document.querySelector("#gf1");
 const draw = document.querySelector("#d1");
@@ -21,39 +25,31 @@ const score = document.querySelector(".scores");
 const team1 = document.querySelector("#team--wale");
 const team2 = document.querySelector("#team--kola");
 
+const       wk      = document.querySelector("#wk")
+    ,       dt      = document.querySelector("#dt")
+    ,       tm1     = document.querySelector("#tm1")
+    ,       tm2     = document.querySelector("#tm2")
+    ,       sc1     = document.querySelector("#sc1")
+    ,       sc2     = document.querySelector("#sc2")
+    ,       del     = document.querySelector("#del")
+    ;
 
-const weekly = document.querySelector("#week");
-const dat = document.querySelector("#dat");
-const t11 = document.querySelector("#tea1");
-const t12 = document.querySelector("#tea2");
-const scoresheet = document.querySelector("#scoretd")
-console.log(team1);
-console.log(team2);
-console.log(typeof Number(team1.value));
+
 
 const setScore = document.querySelector("#set-scores");
-console.log(setScore);
 
 setScore.addEventListener("click", (e) =>{
-    console.log("Button was clicked!");
     let newScore1 = Number(team1.value);
-    console.log(newScore1);
     let newScore2 = Number(team2.value);
-    console.log(newScore2);
 
     let matchPlayed = Number(played.textContent);
-    console.log(matchPlayed); 
 
     played.textContent = matchPlayed + 1;
-    console.log(played.textContent);
     
     let matchPlayed2 = Number(played2.textContent);
-    console.log(matchPlayed2);
     
     played2.textContent = matchPlayed2 + 1;
-    console.log(played2.textContent);
     let newWin = Number(win.textContent);
-    console.log(win.textContent);
 
     // let weekcount = Number(weekly.textContent);
     // weekly.textContent = weekcount(Number(length + 1));
@@ -86,15 +82,12 @@ setScore.addEventListener("click", (e) =>{
     // goals for
     let goalfor1 = Number(goals.textContent);
     goals.textContent = Number(newScore1) + Number(goals.textContent);
-    console.log(goalfor1);
 
     let goalfor2 = Number(goals2.textContent);
     goals2.textContent= Number(newScore2) +  Number(goals2.textContent);
-    console.log(goals2);
     // goal against
     let goalA = Number(against.textContent);
     against.textContent = Number(newScore2) + Number(against.textContent);
-     console.log(goalA);
      let goalB = Number(against2.textContent);
      against2.textContent = Number(newScore1) + Number(against2.textContent);
     //  goals difference
@@ -103,18 +96,35 @@ setScore.addEventListener("click", (e) =>{
 
     let goalDiff2 = Number(differ2.textContent)
     differ2.textContent = Number(goals2.textContent) - Number(against2.textContent);
-    console.log(goalDiff2);
     // points
     let victory = Number(point.textContent);
     point.textContent = (Number(win.textContent) *3) + (Number(draw.textContent));
     let victory2 = Number(point2.textContent);
     point2.textContent =(Number(win2.textContent)*3) +(Number(draw2.textContent));
 
-    // let weekly = Number(week.textContent);
-    // week.textContent = weekly + 1;
-    // console.log(week.textContent);
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let dateFig = date.getDate();
+    let dateString = `${dateFig}/${month}/${year}`
 
+    // wk.textContent = 1;
+    // dt.textContent = dateString;
+    // tm1.textContent = "Team 1";
+    // tm2.textContent = "Team 2";
+    // sc1.textContent = newScore1;
+    // sc2.textContent = newScore2;
+    let tr = document.createElement("tr");
     
+    for(let i=0; i<7; i++){
+        let td = document.createElement("td");
+        td.textContent = `Believe ${i}`;
+        tr.append(td);
+    }
+
+    console.log(tr);
+    tbody.append(tr);
+
 })
 
 
