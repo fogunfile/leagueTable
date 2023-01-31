690ab5019e70e8b53d33eaf02a37d981b91a7f1e
Foluso Ogunfile <folusoogunfile@yahoo.com>
2023-01-31 11:42:21 +0100

    change DB

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ mongoose.set('strictQuery', false);
 // Connect to mongodb
 const connect = function () {
     const options = {useNewUrlParser: true, useUnifiedTopology: true };
-    mongoose.connect(process.env.DB_team, options);
+    mongoose.connect(process.env.DB, options);
     // console.log("connected to db")
 };
 connect();
